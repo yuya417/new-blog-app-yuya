77db911f28ad6bdab6c6998a93589edf0c3358ba
鎌田裕也 <ykprogramdiv417@gmail.com>
2020-10-25 15:33:33 +0900

    Day18終了

--- app/models/article.rb
+++ app/models/article.rb
@@ -10,9 +10,26 @@
 #
 class Article < ApplicationRecord
   validates :title, presence: true
+  validates :title, length: {minimum: 2, maximum: 100}
+  validates :title, format: {with: /\A(?!\@)/}
+
   validates :content, presence: true
+  validates :content, length: {minimum: 10}
+  validates :content, uniqueness: true
+
+  validate :validate_title_and_content_length
 
   def display_created_at
     I18n.l(self.created_at, format: :default)
   end
+
+
+  private
+  def validate_title_and_content_length
+    char_count = self.title.length + self.content.length
+    unless char_count > 30
+      errors.add(:content, 'は30文字以上で投稿できます！')
+    end
+  end
+
 end
