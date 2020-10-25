8a76996c7be7b2422a4ad538f6af80e9311173e6
鎌田裕也 <ykprogramdiv417@gmail.com>
2020-10-25 11:43:12 +0900

    Day 16 終了

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-50.times do 
+50.times do
   Article.create(
     title: Faker::Lorem.sentence(word_count: 5),
     content: Faker::Lorem.sentence(word_count: 100)
