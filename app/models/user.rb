cc61a0bdd8dfba7572bc412c6f0b19edf2b2c47e
鎌田裕也 <ykprogramdiv417@gmail.com>
2020-10-28 00:30:17 +0900

    Day25終了

--- app/models/user.rb
+++ app/models/user.rb
@@ -24,12 +24,18 @@ class User < ApplicationRecord
 
 
   has_many :articles, dependent: :destroy
+  has_many :likes, dependent: :destroy
+  has_many :favorite_articles, through: :likes, source: :article
   has_one :profile, dependent: :destroy
 
   delegate :birthday,:age, :gender, to: :profile, allow_nil: true
 
   def has_written?(article)
     articles.exists?(id: article.id)
+  end
+
+  def has_liked?(article)
+    likes.exists?(article_id: article.id)
   end
 
   def display_name
