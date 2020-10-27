64e96edd17dd6e9e7f06304b935026f7c10cbacb
鎌田裕也 <ykprogramdiv417@gmail.com>
2020-10-27 14:27:58 +0900

    Day24終了

---
+++ app/controllers/profiles_controller.rb
@@ -0,0 +1,36 @@
+class ProfilesController < ApplicationController
+  before_action :authenticate_user!
+
+  def show
+    @profile = current_user.profile
+  end
+
+  def edit
+    @profile = current_user.prepare_profile
+  end
+
+  def update
+    @profile = current_user.prepare_profile
+    @profile.assign_attributes(profile_params)
+    if @profile.save
+      redirect_to profile_path, notice: 'プロフィールを更新しました！'
+    else
+      flash.now[:error] = '更新できませんでした'
+      render :edit
+    end
+    
+  end
+
+  private
+  def profile_params
+    params.require(:profile).permit(
+      :nickname,
+      :introduction,
+      :gender,
+      :birthday,
+      :subscribed,
+      :avatar
+    )
+  end
+
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -24,13 +24,36 @@ class User < ApplicationRecord
 
 
   has_many :articles, dependent: :destroy
+  has_one :profile, dependent: :destroy
+
+  delegate :birthday,:age, :gender, to: :profile, allow_nil: true
 
   def has_written?(article)
     articles.exists?(id: article.id)
   end
 
   def display_name
-    self.email.split('@').first
+    profile&.nickname || self.email.split('@').first
+  end
+
+  # def birthday
+  #   profile&.birthday
+  # end
+
+  # def gender
+  #   profile&.gender
+  # end
+
+  def prepare_profile
+    profile || build_profile
+  end
+
+  def avatar_image
+    if profile&.avatar&.attached?
+      profile.avatar
+    else
+      'default-avatar.png'
+    end
   end
 
 
